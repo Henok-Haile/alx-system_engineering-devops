99e2d5a75cd831d356f8738a685807f5c36ad45c
Henok-Haile <henokhaile53@gmail.com>
2023-09-27 02:33:38 -0700

    Task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,10 @@
 # configuring nginx using puppet automation
 
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
 package { 'nginx':
   ensure => installed,
 }
@@ -8,11 +13,16 @@ file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewirte ^/redirect_me https://www.github.com/Henok-Haile permanent;'.
+  line   => 'rewirte ^/redirect_me https://www.github.com/Henok-Haile permanent;',
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
+}
+
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 
 service { 'nginx':
