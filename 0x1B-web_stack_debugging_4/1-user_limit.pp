69658028ad21680fcc4ea4d41f50c394d38e8e73
Henok-Haile <henokhaile53@gmail.com>
2024-05-17 10:13:43 +0000

    Task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,14 @@
+# Enbale the user holberton to login and open a files with out error
+
+# Increase hard file limit for holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => "sed -i '/^holberton.*hard.*nofile/s/[0-9]\\+/50000/' /etc/security/limits.conf || echo 'holberton hard nofile 50000' >> /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin:/usr/bin',
+}
+
+# Increase soft file limit for holberton user
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => "sed -i '/^holberton.*soft.*nofile/s/[0-9]\\+/50000/' /etc/security/limits.conf || echo 'holberton soft nofile 50000' >> /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin:/usr/bin',
+}
+
